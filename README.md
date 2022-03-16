1cc18278e2c54c107ae91ac023fdb88500090cda
Stephan Rossi <stephanrossi@previsaassessoria.com.br>
2022-03-16 12:57:27 -0300

    All installed

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-<h2>Projeto loterias</h2><br>
+<h1>Projeto loterias</h1><br>
 
 <p>Uso de API pública para a consulta de resultados de loterias da Caixa</p><br>
 
 <span>O objetivo do projeto é criar um sistema para o cadastro de jogos feitos e também para a consulta de loterias da
-    Caixa, cruzando os dados para saber o quantidade de números acertados.</span>
+    Caixa, cruzando os dados para saber o quantidade de números acertados.
+    Tudo feito para fins de estudo.
+</span>
 
 <span>Foram utilizados neste sistema:</span>
 
---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express from "express"
+import dotenv from "dotenv"
+import mustache from "mustache-express"
+import path from "path"
+import router from './routes'
+
+//Initialize DOTENV
+dotenv.config()
+
+//Initialize express
+const app = express()
+
+//Set view engine
+app.set("view engine", "mst")
+app.set("views", path.join(__dirname, "views"))
+app.engine("mst", mustache())
+
+//Set public folder
+app.use(express.static(path.join(__dirname, "../public")))
+
+//Routes
+app.use(router)
+
+//404
+app.use((req, res) => {
+    res.render("pages/404")
+})
+
+//Start server
+app.listen(process.env.PORT || 3000)
+console.log("\n" + "Server running" + "\n")
